5d80c9e2fff3348256a480ed07b02d48f9e8d52d
leitanglong03 <leitanglong03@kuaishou.com>
2023-11-15 23:51:59 +0800

    Initial Commit

---
+++ Learn2048/Learn2048App.swift
@@ -0,0 +1,17 @@
+//
+//  Learn2048App.swift
+//  Learn2048
+//
+//  Created by leitanglong on 2023/11/15.
+//
+
+import SwiftUI
+
+@main
+struct Learn2048App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
